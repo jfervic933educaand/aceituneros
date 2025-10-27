7148939af6f65347fb02414f1de55b48c29dd369
Vico <jfervic933@g.educaand.es>
2025-10-27 12:01:58 +0100

    Mensaje inicial en clase Mensajes

--- src/main/java/alboran/Main.java
+++ src/main/java/alboran/Main.java
@@ -2,6 +2,13 @@ package alboran;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int opcion = 0;
+
+        do{
+
+            System.out.println(Mensajes.menuInicial);
+
+        } while (opcion!=2);
     }
 }
