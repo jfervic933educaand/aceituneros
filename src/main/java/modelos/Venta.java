657cb7960bf99a5f37866f93309a3d80173bb4f1
Vico <jfervic933@g.educaand.es>
2025-11-24 11:07:13 +0100

    Clase Venta con constructor, getters, setters y toString

--- src/main/java/modelos/Venta.java
+++ src/main/java/modelos/Venta.java
@@ -7,14 +7,14 @@ Encapsula información relacionada
 // Declaración -> public class Nombre
 public class Venta {
 
-    // Atributos de instancia
+    // Atributos de instancia - privados
     // Los atributos se inicializan con null (objetos), 0 números, 
     // false booleanos como valores por defecto
-    String nombreProductor;
-    String tamanioAceituna;
-    String tipoAceituna;
-    double kg;
-    double precioVenta;
+    private String nombreProductor;
+    private String tamanioAceituna;
+    private String tipoAceituna;
+    private double kg;
+    private double precioVenta;
 
     // Métodos de instancia
     // Constructor
@@ -22,21 +22,60 @@ public class Venta {
     // El constructor siempre se llama igual que la clase
     // Nunca especifican dato de retorno
     // Pasamos como parámetro los valores que queremos poner al objeto
-    public Venta(String nombre, String tamanio, String tipo, double kg){
-        // Usando el puntero this indico que el atributo de este objeto
-        // tiene el valor del parámetro
-        this.nombreProductor = nombre;
-        this.tamanioAceituna = tamanio;
-        this.tipoAceituna = tipo;
+
+    public Venta(String nombre, String tamanioAceituna, String tipoAceituna, double kg, double precioVenta) {
+        nombreProductor = nombre;
+        this.tamanioAceituna = tamanioAceituna;
+        this.tipoAceituna = tipoAceituna;
+        this.kg = kg;
+        this.precioVenta = precioVenta;
+    }
+
+    public String getNombreProductor() {
+        return nombreProductor;
+    }
+
+    public void setNombreProductor(String nombreProductor) {
+        this.nombreProductor = nombreProductor;
+    }
+
+    public String getTamanioAceituna() {
+        return tamanioAceituna;
+    }
+
+    public void setTamanioAceituna(String tamanioAceituna) {
+        this.tamanioAceituna = tamanioAceituna;
+    }
+
+    public String getTipoAceituna() {
+        return tipoAceituna;
+    }
+
+    public void setTipoAceituna(String tipoAceituna) {
+        this.tipoAceituna = tipoAceituna;
+    }
+
+    public double getKg() {
+        return kg;
+    }
+
+    public void setKg(double kg) {
         this.kg = kg;
     }
 
-    public void mostrarValores(){
-        System.out.println("Nombre: " + this.nombreProductor + 
-        " tamaño: " + this.tamanioAceituna + 
-        " tipo: " + this.tipoAceituna + 
-        " kg: " + this.kg + 
-        " precioVenta: " + this.precioVenta); 
+    public double getPrecioVenta() {
+        return precioVenta;
     }
 
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    // Anotación informativa de sobrescritura de método
+    @Override
+    public String toString() {
+        return "Venta {nombreProductor=" + nombreProductor + ", tamanioAceituna=" + tamanioAceituna + ", tipoAceituna="
+                + tipoAceituna + ", kg=" + kg + ", precioVenta=" + precioVenta + "}";
+    }
+   
 }
